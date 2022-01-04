9ad3ba6d05aab857be5090d469d7f6f99973db45
Lorenzo Todisco <lorenzo.todisco@studio.unibo.it>
2022-01-04 19:30:28 +0100

    Added 'Remove at index button'

--- src/base/LampGUI.java
+++ src/base/LampGUI.java
@@ -8,12 +8,16 @@ import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 /**
  * GUI for LampModel.
@@ -31,12 +35,15 @@ public class LampGUI {
     /**
      * Constructor for the GUI.
      */
+    @SuppressWarnings("PMD.ReplaceVectorWithList")
     public LampGUI() {
 
         final JPanel canvas = new JPanel();
         final JPanel buttons = new JPanel();
         final JPanel lamps = new JPanel();
         final List<Pair<LampModel, JPanel>> lampList = new LinkedList<>();
+        @SuppressWarnings("PMD.UseArrayListInsteadOfVector")
+        final Vector<Integer> index = new Vector<>();
 
         final var newLamp = new MyButton("Create New Lamp");
         newLamp.setHorizontalAlignment(JButton.CENTER);
@@ -49,10 +56,28 @@ public class LampGUI {
         final var removeLastLamp = new MyButton("Remove Last Lamp");
         removeLastLamp.setHorizontalAlignment(JButton.CENTER);
         removeLastLamp.setFont(new Font(FONT, Font.BOLD, 32));
-        removeLastLamp.setBackground(new Color(0, 0, 255));
-        removeLastLamp.setHoverBackgroundColor(new Color(0, 0, 255));
-        removeLastLamp.setPressedBackgroundColor(new Color(0, 0, 120));
+        removeLastLamp.setBackground(new Color(0, 181, 222));
+        removeLastLamp.setHoverBackgroundColor(new Color(0, 181, 222));
+        removeLastLamp.setPressedBackgroundColor(new Color(0, 140, 171));
         removeLastLamp.setFocusPainted(false);
+
+        final var removeSelectedLamp = new MyButton("Remove At Selected Index");
+        removeSelectedLamp.setHorizontalAlignment(JButton.LEFT);
+        removeSelectedLamp.setFont(new Font(FONT, Font.BOLD, 32));
+        removeSelectedLamp.setBackground(new Color(0, 181, 222));
+        removeSelectedLamp.setHoverBackgroundColor(new Color(0, 181, 222));
+        removeSelectedLamp.setPressedBackgroundColor(new Color(0, 140, 171));
+        removeSelectedLamp.setFocusPainted(false);
+        removeSelectedLamp.setLayout(new GridLayout(1, 2));
+        removeSelectedLamp.add(new JPanel());
+        removeSelectedLamp.getComponent(0).setVisible(false);
+
+        final var indexSelector = new JComboBox<Integer>(index);
+        indexSelector.setFont(new Font(FONT, Font.BOLD, 54));
+        ((JLabel) indexSelector.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+        indexSelector.setFocusable(false);
+        indexSelector.setBackground(new Color(0, 181, 222));
+        removeSelectedLamp.add(indexSelector);
 
         final var allOff = new MyButton("Turn Off All Lamps");
         allOff.setHorizontalAlignment(JButton.CENTER);
@@ -71,18 +96,19 @@ public class LampGUI {
         allOn.setFocusPainted(false);
 
         canvas.setLayout(new GridLayout(1, 2));
-        buttons.setLayout(new GridLayout(4, 1));
+        buttons.setLayout(new GridLayout(5, 1));
         lamps.setLayout(new FlowLayout());
 
         buttons.add(newLamp);
         buttons.add(removeLastLamp);
+        buttons.add(removeSelectedLamp);
         buttons.add(allOn);
         buttons.add(allOff);
         canvas.add(buttons);
 
         // Utility Classes
 
-        class SwitchLamp {
+        class LampOperations {
             void switchLamp(final LampModel lamp, final JPanel pan) {
                 lamp.invertState();
                 pan.remove(0);
@@ -111,13 +137,36 @@ public class LampGUI {
                     pan.revalidate();
                 }
             }
+
+            void removeAtIndex(final List<Integer> indexVector, final List<Pair<LampModel, JPanel>> lampList,
+                    final JPanel lamps, final JComboBox<Integer> indexSelector, final int index) {
+                if (lampCount > 0) {
+                    if (index < MAX_LAMPS) {
+                        indexVector.remove(--lampCount);
+
+                        lampList.remove(index);
+                        lamps.remove(index);
+                        indexSelector.revalidate();
+                        indexSelector.setSelectedIndex(
+                                indexSelector.getSelectedIndex() == lampCount ? -1 : indexSelector.getSelectedIndex());
+                        lamps.revalidate();
+                        lamps.repaint();
+                    } else {
+                        JOptionPane.showMessageDialog(f, "Invalid Index!", "Invalid Index Error",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(f, "Can't remove any more lamps!", "Too Little Lamps Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
         }
 
         // Button Operations
 
         newLamp.addActionListener(e -> {
             if (lampCount < MAX_LAMPS) {
-                lampCount++;
+                index.add(++lampCount);
 
                 final JPanel lampLayout = new JPanel();
                 final var lamp = new LampModel();
@@ -137,33 +186,28 @@ public class LampGUI {
                 final Pair<LampModel, JPanel> pair = new Pair<>(lamp, lampLayout);
                 lampList.add(pair);
 
-                lampSwitch.addActionListener(a -> new SwitchLamp().switchLamp(lamp, lampLayout));
+                lampSwitch.addActionListener(a -> new LampOperations().switchLamp(lamp, lampLayout));
 
+                indexSelector.revalidate();
                 lamps.revalidate();
+                lamps.repaint();
             } else {
                 JOptionPane.showMessageDialog(f, "Can't insert any more lamps!", "Too Many Lamps Error",
                         JOptionPane.ERROR_MESSAGE);
             }
         });
 
-        removeLastLamp.addActionListener(e -> {
-            if (lampCount > 0) {
-                lampCount--;
-                lampList.remove(lampList.size() - 1);
-                lamps.remove(lamps.getComponentCount() - 1);
-                lamps.revalidate();
-                lamps.repaint();
-            } else {
-                JOptionPane.showMessageDialog(f, "Can't remove any more lamps!", "Too Little Lamps Error",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        });
+        removeLastLamp.addActionListener(e -> new LampOperations().removeAtIndex(index, lampList, lamps, indexSelector,
+                lamps.getComponentCount() - 1));
+
+        removeSelectedLamp.addActionListener(e -> new LampOperations().removeAtIndex(index, lampList, lamps, indexSelector,
+                indexSelector.getSelectedIndex() >= 0 ? indexSelector.getSelectedIndex() : lamps.getComponentCount() - 1));
 
         allOn.addActionListener(
-                e -> lampList.stream().forEach(pair -> new SwitchLamp().turnOn(pair.getFirst(), pair.getSecond())));
+                e -> lampList.stream().forEach(pair -> new LampOperations().turnOn(pair.getFirst(), pair.getSecond())));
 
-        allOff.addActionListener(
-                e -> lampList.stream().forEach(pair -> new SwitchLamp().turnOff(pair.getFirst(), pair.getSecond())));
+        allOff.addActionListener(e -> lampList.stream()
+                .forEach(pair -> new LampOperations().turnOff(pair.getFirst(), pair.getSecond())));
 
         // Frame Operations
         canvas.add(lamps);
